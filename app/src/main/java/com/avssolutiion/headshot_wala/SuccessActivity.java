219b87bbc6d4421ebdc109cdf2907d7be8e021a9
zeb <aryan746505@gmail.com>
2023-01-21 08:11:53 -0800

    Final_commit

--- app/src/main/java/com/avssolutiion/headshot_wala/SuccessActivity.java
+++ app/src/main/java/com/avssolutiion/headshot_wala/SuccessActivity.java
@@ -3,12 +3,26 @@ package com.avssolutiion.headshot_wala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.avssolutiion.headshot_wala.databinding.ActivitySuccessBinding;
 
 public class SuccessActivity extends AppCompatActivity {
+
+    ActivitySuccessBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_success);
+        binding = ActivitySuccessBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
